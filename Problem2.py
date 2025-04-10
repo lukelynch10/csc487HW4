a73e1b35c56140c185e0505347a556feb39b244f
Luke Lynch <156872631+lukelynch10@users.noreply.github.com>
2025-04-09 23:41:25 -0400

    Add files via upload

--- Problem2.py
+++ Problem2.py
@@ -2,9 +2,8 @@ from sklearn import svm
 import pandas as pd
 import numpy as np
 
-C=1e+03 #1000
-gamma=1e-05 #0.0001
-clf = svm.SVC(C=C, gamma=gamma, kernel='rbf', probability=True)
-
-train = pd.read_csv('/diabetes_train.csv') #this is our dataset, please change the path for your case
+train = pd.read_csv('/diabetes_train.csv') 
 test = pd.read_csv('/diabetes_test.csv')
+
+for i, chunk in enumerate(pd.read_csv(train, chunksize=2)):
+        chunk.to_csv(f'{'train_{i}'}_{i+1}.csv', index=False)
